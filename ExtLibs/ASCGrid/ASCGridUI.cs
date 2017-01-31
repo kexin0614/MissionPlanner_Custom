b8f5a839f4efb9fc2213fed597b41be665e7c61a
mzm0018 <913830677@qq.com>
2017-01-31 21:12:34 +0800

    In better order

--- ExtLibs/ASCGrid/ASCGridUI.cs
+++ ExtLibs/ASCGrid/ASCGridUI.cs
@@ -35,15 +35,10 @@ namespace MissionPlanner
             this.plugin = plugin;
 
             InitializeComponent();
-
-            myGMAP1.MapProvider = plugin.Host.FDMapType;   //选择host使用的地图提供商
-            // myGMAP1.Position = new GMap.NET.PointLatLng(31.027964, 121.439399);   //致远湖的坐标
-
-            myGMAP1.OnPositionChanged += myGMAP1_OnCurrentPositionChanged; //运行myGMAP1_OnCurrentPositionChanged函数
-            myGMAP1.Overlays.Add(FlightPlanner.airportsoverlay); //增加机场
+            
         }
 
-        void myGMAP1_OnCurrentPositionChanged(PointLatLng point) //现有功能是增加机场的Marker
+        void myGMAP1_OnCurrentPositionChanged(PointLatLng point)  //现有功能是增加机场的Marker
         {
             FlightPlanner.airportsoverlay.Clear();
             foreach (var item in Airports.getAirports(myGMAP1.Position))
@@ -58,8 +53,13 @@ namespace MissionPlanner
 
         private void ASCGridUI_Load(object sender, EventArgs e)
         {
+            myGMAP1.MapProvider = plugin.Host.FDMapType;   //选择host使用的地图提供商
+
             ascroutesOverlay = new GMapOverlay("ascroutesOverlay");  //id为ascroutesOverlay的路径图层
             myGMAP1.Overlays.Add(ascroutesOverlay);  //增加图层
+
+            myGMAP1.OnPositionChanged += myGMAP1_OnCurrentPositionChanged; //运行myGMAP1_OnCurrentPositionChanged函数
+            myGMAP1.Overlays.Add(FlightPlanner.airportsoverlay); //增加机场
 
             var points = plugin.Host.FPDrawnPolygon;
             points.Points.ForEach(x => { asclist.Add(x); });  //将多边形信息记录到asclist中
@@ -72,7 +72,7 @@ namespace MissionPlanner
 
         private void FindingASCGrid(object sender, EventArgs e)   //计算航点
         {
-
+            //ASCCreateGrid
         }
 
         void DrawPolygon()    //绘制多边形边界和顶点
@@ -98,7 +98,7 @@ namespace MissionPlanner
         }
         private void ASCGridUI_Resize(object sender, EventArgs e)  
         {
-            myGMAP1.ZoomAndCenterMarkers(ascroutesOverlay.Id);
+            myGMAP1.ZoomAndCenterMarkers(ascroutesOverlay.Id);    //在改变窗口大小时，依旧保持zoom和中心在合适位置
         }
     }
 }
